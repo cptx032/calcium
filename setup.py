8b912bc1b47aa48c769829401eb062a40eb57b0d
Willie Lawrence <cptx032@gmail.com>
2018-03-23 16:10:11 -0300

    #17 - Creating a setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='calcium',
+    version='0.1.0',
+    packages=['calcium'],
+    description='A terminal game framework',
+    author='Willie Lawrence',
+    author_email='cptx032@gmail.com',
+    url='https://github.com/cptx032/calcium/',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Topic :: Games/Entertainment',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3 :: Only'
+    ])
