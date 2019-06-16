5dade09857f29fdd4547bf256c5ccec30e95d96d
Cptx032 <cptx032@gmail.com>
2019-06-16 10:56:15 -0300

    pip compatible

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
         'Programming Language :: Python :: 3 :: Only'
     ],
     install_requires=[
-        'Pillow=5.0.0',
-        'PyTweening=1.0.3'
+        'Pillow==5.0.0',
+        'PyTweening==1.0.3'
     ],)
